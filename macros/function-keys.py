a6e358c16383ec955cae6ebf534db7d50b5d92b4
Matt Nicolay <mnicolay@solstice.com>
2021-08-29 16:35:19 -0500

    Change some colors

--- macros/function-keys.py
+++ macros/function-keys.py
@@ -5,21 +5,21 @@ app = {                    # REQUIRED dict, must be named 'app'
     'macros' : [           # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x260021, 'F10', [Keycode.F10]),
-        (0x260021, 'F11', [Keycode.F11]),
-        (0x260021, 'F12', [Keycode.F12]),
+        (0x00261a, 'F10', [Keycode.F10]),
+        (0x00261a, 'F11', [Keycode.F11]),
+        (0x00261a, 'F12', [Keycode.F12]),
         # 2nd row ----------
-        (0x260017, 'F7', [Keycode.F7]),
-        (0x260017, 'F8', [Keycode.F8]),
-        (0x260017, 'F9', [Keycode.F9]),
+        (0x002622, 'F7', [Keycode.F7]),
+        (0x002622, 'F8', [Keycode.F8]),
+        (0x002622, 'F9', [Keycode.F9]),
         # 3rd row ----------
-        (0x26000d, 'F4', [Keycode.F4]),
-        (0x26000d, 'F5', [Keycode.F5]),
-        (0x26000d, 'F6', [Keycode.F6]),
+        (0x002625, 'F4', [Keycode.F4]),
+        (0x002625, 'F5', [Keycode.F5]),
+        (0x002625, 'F6', [Keycode.F6]),
         # 4th row ----------
-        (0x260008, 'F1', [Keycode.F1]),
-        (0x260008, 'F2', [Keycode.F2]),
-        (0x260008, 'F3', [Keycode.F3]),
+        (0x002026, 'F1', [Keycode.F1]),
+        (0x002026, 'F2', [Keycode.F2]),
+        (0x002026, 'F3', [Keycode.F3]),
         # Encoder button ---
         (0x000000, '', [Keycode.BACKSPACE])
     ]
--- macros/mac-default.py
+++ macros/mac-default.py
@@ -9,21 +9,21 @@ app = {                      # REQUIRED dict, must be named 'app'
     'macros' : [             # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x018FC2, 'Scrnsht', [Keycode.COMMAND, Keycode.SHIFT, '3']),
-        (0x018FC2, 'Left', [Keycode.CONTROL, Keycode.LEFT_ARROW]),
-        (0x018FC2, 'Right', [Keycode.CONTROL, Keycode.RIGHT_ARROW]),
+        (0x260021, 'Scrnsht', [Keycode.COMMAND, Keycode.SHIFT, '3']),
+        (0x260021, 'Left', [Keycode.CONTROL, Keycode.LEFT_ARROW]),
+        (0x260021, 'Right', [Keycode.CONTROL, Keycode.RIGHT_ARROW]),
         # 2nd row ----------
-        (0x0829C2, 'Emoji', [Keycode.CONTROL, Keycode.COMMAND, ' ']),
-        (0x0829C2, 'Brt Dn', [ConsumerControlCode.BRIGHTNESS_DECREMENT]),
-        (0x0829C2, 'Brt Up', [ConsumerControlCode.BRIGHTNESS_INCREMENT]),
+        (0x260017, 'Emoji', [Keycode.CONTROL, Keycode.COMMAND, ' ']),
+        (0x260017, 'Brt Dn', [ConsumerControlCode.BRIGHTNESS_DECREMENT]),
+        (0x260017, 'Brt Up', [ConsumerControlCode.BRIGHTNESS_INCREMENT]),
         # 3rd row ----------
-        (0xAB029A, 'Mute', [ConsumerControlCode.MUTE]),
-        (0xAB029A, 'Vol Down', [ConsumerControlCode.VOLUME_DECREMENT]),
-        (0xAB029A, 'Vol Up', [ConsumerControlCode.VOLUME_INCREMENT]),
+        (0x26000d, 'Mute', [ConsumerControlCode.MUTE]),
+        (0x26000d, 'Vol Down', [ConsumerControlCode.VOLUME_DECREMENT]),
+        (0x26000d, 'Vol Up', [ConsumerControlCode.VOLUME_INCREMENT]),
         # 4th row ----------
-        (0x4D00AB, 'Prev', [ConsumerControlCode.SCAN_PREVIOUS_TRACK ]),
-        (0x4D00AB, 'Pause', [ConsumerControlCode.PLAY_PAUSE]),
-        (0x4D00AB, 'Next', [ConsumerControlCode.SCAN_NEXT_TRACK]),
+        (0x260008, 'Prev', [ConsumerControlCode.SCAN_PREVIOUS_TRACK ]),
+        (0x260008, 'Pause', [ConsumerControlCode.PLAY_PAUSE]),
+        (0x260008, 'Next', [ConsumerControlCode.SCAN_NEXT_TRACK]),
         # Encoder button ---
         (0x000000, '', [Keycode.CONTROL, Keycode.UP_ARROW]) # Close window/tab
     ]
