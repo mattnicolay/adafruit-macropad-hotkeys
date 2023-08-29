9a81500d9f2879745e67654d57afd9e1fd961fde
Matt Nicolay <mnicolay@solstice.com>
2023-08-29 10:05:52 -0500

    Update colors in macro.py

--- macros/macro.py
+++ macros/macro.py
@@ -5,21 +5,21 @@ app = {                    # REQUIRED dict, must be named 'app'
     'macros' : [           # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x00261a, 'F20', [Keycode.F20]),
-        (0x00261a, 'F21', [Keycode.F21]),
-        (0x00261a, 'F22', [Keycode.F22]),
+        (0x260021, 'F20', [Keycode.F20]),
+        (0x260021, 'F21', [Keycode.F21]),
+        (0x260021, 'F22', [Keycode.F22]),
         # 2nd row ----------
-        (0x002622, 'F17', [Keycode.F17]),
-        (0x002622, 'F18', [Keycode.F18]),
-        (0x002622, 'F19', [Keycode.F19]),
+        (0x260017, 'F17', [Keycode.F17]),
+        (0x260017, 'F18', [Keycode.F18]),
+        (0x260017, 'F19', [Keycode.F19]),
         # 3rd row ----------
-        (0x002625, 'F14', [Keycode.F14]),
-        (0x002625, 'F15', [Keycode.F15]),
-        (0x002625, 'F16', [Keycode.F16]),
+        (0x26000d, 'F14', [Keycode.F14]),
+        (0x26000d, 'F15', [Keycode.F15]),
+        (0x26000d, 'F16', [Keycode.F16]),
         # 4th row ----------
-        (0x002026, 'F11', [Keycode.F11]),
-        (0x002026, 'F12', [Keycode.F12]),
-        (0x002026, 'F13', [Keycode.F13]),
+        (0x260008, 'F11', [Keycode.F11]),
+        (0x260008, 'F12', [Keycode.F12]),
+        (0x260008, 'F13', [Keycode.F13]),
         # Encoder button ---
         (0x000000, '', [Keycode.F23])
     ]
