7203401e636b3e2899fc3a47fa2f9feec1665376
Matt Nicolay <mnicolay@solstice.com>
2023-08-29 10:16:33 -0500

    Update colors in macro and numpad

--- macros/macro.py
+++ macros/macro.py
@@ -5,21 +5,21 @@ app = {                    # REQUIRED dict, must be named 'app'
     'macros' : [           # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x260021, 'F20', [Keycode.F20]),
-        (0x260021, 'F21', [Keycode.F21]),
-        (0x260021, 'F22', [Keycode.F22]),
+        (0x260017, 'F20', [Keycode.F20]),
+        (0x260008, 'F21', [Keycode.F21]),
+        (0x260008, 'F22', [Keycode.F22]),
         # 2nd row ----------
-        (0x260017, 'F17', [Keycode.F17]),
+        (0x26000d, 'F17', [Keycode.F17]),
         (0x260017, 'F18', [Keycode.F18]),
-        (0x260017, 'F19', [Keycode.F19]),
+        (0x260008, 'F19', [Keycode.F19]),
         # 3rd row ----------
-        (0x26000d, 'F14', [Keycode.F14]),
+        (0x260021, 'F14', [Keycode.F14]),
         (0x26000d, 'F15', [Keycode.F15]),
-        (0x26000d, 'F16', [Keycode.F16]),
+        (0x260017, 'F16', [Keycode.F16]),
         # 4th row ----------
-        (0x260008, 'F11', [Keycode.F11]),
-        (0x260008, 'F12', [Keycode.F12]),
-        (0x260008, 'F13', [Keycode.F13]),
+        (0x260021, 'F11', [Keycode.F11]),
+        (0x260021, 'F12', [Keycode.F12]),
+        (0x26000d, 'F13', [Keycode.F13]),
         # Encoder button ---
         (0x000000, '', [Keycode.F23])
     ]
--- macros/numpad.py
+++ macros/numpad.py
@@ -7,21 +7,21 @@ app = {                    # REQUIRED dict, must be named 'app'
     'macros' : [           # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x0d2600, '7', [Keycode.KEYPAD_SEVEN]),
-        (0x0d2600, '8', [Keycode.KEYPAD_EIGHT]),
-        (0x0d2600, '9', [Keycode.KEYPAD_NINE]),
+        (0x120026, '7', [Keycode.KEYPAD_SEVEN]),
+        (0x120026, '8', [Keycode.KEYPAD_EIGHT]),
+        (0x120026, '9', [Keycode.KEYPAD_NINE]),
         # 2nd row ----------
-        (0x0d2600, '4', [Keycode.KEYPAD_FOUR]),
-        (0x0d2600, '5', [Keycode.KEYPAD_FIVE]),
-        (0x0d2600, '6', [Keycode.KEYPAD_SIX]),
+        (0x120026, '4', [Keycode.KEYPAD_FOUR]),
+        (0x120026, '5', [Keycode.KEYPAD_FIVE]),
+        (0x120026, '6', [Keycode.KEYPAD_SIX]),
         # 3rd row ----------
-        (0x0d2600, '1', [Keycode.KEYPAD_ONE]),
-        (0x0d2600, '2', [Keycode.KEYPAD_TWO]),
-        (0x0d2600, '3', [Keycode.KEYPAD_THREE]),
+        (0x120026, '1', [Keycode.KEYPAD_ONE]),
+        (0x120026, '2', [Keycode.KEYPAD_TWO]),
+        (0x120026, '3', [Keycode.KEYPAD_THREE]),
         # 4th row ----------
-        (0x101010, '-', [Keycode.KEYPAD_MINUS]),
-        (0x0d2600, '0', [Keycode.KEYPAD_ZERO]),
-        (0x101010, '+', [Keycode.KEYPAD_PLUS]),
+        (0x004000, '-', [Keycode.KEYPAD_MINUS]),
+        (0x120026, '0', [Keycode.KEYPAD_ZERO]),
+        (0x004000, '+', [Keycode.KEYPAD_PLUS]),
         # Encoder button ---
         (0x000000, '', [Keycode.KEYPAD_ENTER])
     ]
